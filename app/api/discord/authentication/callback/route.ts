ca7ffecd6d9413b28a72f1f7136250d45bbf106a
BosEriko <bos@eriko.ph>
2026-03-07 17:16:04 +0800

    Feature: Create an empty generate_custom_token

--- app/api/discord/authentication/callback/route.ts
+++ app/api/discord/authentication/callback/route.ts
@@ -1,4 +1,27 @@
 import { NextResponse } from "next/server";
+// import FirebaseAdmin from "./FirebaseAdmin";
+
+// TODO: Move to its own file
+type User = {
+  id: string;
+  username: string;
+  avatar: string;
+  discriminator: string;
+  public_flags: number;
+  flags: number;
+  banner: string | null;
+  accent_color: number;
+  global_name: string;
+  avatar_decoration_data: unknown | null;
+  collectibles: unknown | null;
+  display_name_styles: unknown | null;
+  banner_color: string | null;
+  clan: unknown | null;
+  primary_guild: unknown | null;
+  mfa_enabled: boolean;
+  locale: string;
+  premium_type: number;
+};
 
 // TODO: Move to its own file
 const exchange_code = async (code: string) => {
@@ -20,15 +43,28 @@ const exchange_code = async (code: string) => {
 }
 
 // TODO: Move to its own file
-const fetch_user = async (token) => {
+const fetch_user = async (token: string): Promise<User> => {
   const response = await fetch('https://discord.com/api/users/@me', {
     headers: {
       Authorization: `Bearer ${token}`,
     },
   });
 
-  return response.json();
+  if (!response.ok) {
+    throw new Error(`Failed to fetch user: ${response.statusText}`);
+  }
+
+  const data: User = await response.json();
+  return data;
 };
+
+// TODO: Move to its own file
+// const generate_custom_token = async (user) => {
+//   return firebase_admin.auth().createCustomToken(user.id, {
+//     displayName: user.username,
+//     profileImage: `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png`,
+//   });
+// };
 
 export async function GET(req: Request) {
   const { searchParams } = new URL(req.url);
@@ -40,8 +76,8 @@ export async function GET(req: Request) {
 
   const token = await exchange_code(code);
   const user = await fetch_user(token);
+  // const custom_token = await generate_custom_token(user)
 
-  // TODO: Generate Firebase Token (https://github.com/BosEriko/boteriko/blob/master/app/controllers/Twitch/authentication_callback/generate_custom_token.js)
   console.log(user);
 
   // TODO: Point to /authenticate?=TOKEN then create a component to manage the token (https://github.com/BosEriko/plus/blob/master/src/app/authenticate/page.jsx)
