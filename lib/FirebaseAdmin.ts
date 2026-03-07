f49d022c27d8fb16bc527bd50f702e702b10c019
BosEriko <bos@eriko.ph>
2026-03-07 17:44:16 +0800

    Feature: Update databaseURL to fetch from FIREBASE_PROJECT_ID

--- lib/FirebaseAdmin.ts
+++ lib/FirebaseAdmin.ts
@@ -7,7 +7,7 @@ if (!FirebaseAdmin.apps.length) {
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL!,
       privateKey: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, '\n'),
     }),
-    databaseURL: process.env.FIREBASE_DATABASE_URL!
+    databaseURL: `https://${process.env.FIREBASE_PROJECT_ID!}-default-rtdb.asia-southeast1.firebasedatabase.app/`
   });
 }
 
