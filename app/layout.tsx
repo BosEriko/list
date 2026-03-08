8a598ddc3f04b483b24dc5719f85d93de2237168
BosEriko <bos@eriko.ph>
2026-03-08 08:37:46 +0800

    Feature: Initialize User on RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,20 @@
-import type { Metadata } from "next";
+'use client';
+import { useEffect } from 'react';
+import useAuthStore from '@store/useAuthStore';
 import "./globals.css";
-
-export const metadata: Metadata = {
-  title: "Anime and Manga Listing",
-  description: "Save your anime and manga!",
-};
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const { initAuth } = useAuthStore();
+
+  useEffect(() => {
+    const unsubscribe = initAuth();
+    return () => unsubscribe();
+  }, [initAuth]);
+
   return (
     <html lang="en">
       <body>{children}</body>
