f78f5faa46d5f9bd47f1cb2f399429845b0083a8
BosEriko <bos@eriko.ph>
2026-03-08 08:27:04 +0800

    Feature: Crease useAuthStore

---
+++ store/useAuthStore.ts
@@ -0,0 +1,36 @@
+'use client';
+import { create } from 'zustand';
+import { onAuthStateChanged, signOut } from 'firebase/auth';
+import { auth } from '@lib/Firebase';
+
+const useAuthStore = create((set) => ({
+  user: null,
+  token: null,
+  loading: true,
+
+  initAuth: () => {
+    return onAuthStateChanged(auth, async (firebaseUser) => {
+      if (firebaseUser) {
+        const token = await firebaseUser.getIdToken();
+        set({
+          user: firebaseUser,
+          token,
+          loading: false,
+        });
+      } else {
+        set({
+          user: null,
+          token: null,
+          loading: false,
+        });
+      }
+    });
+  },
+
+  logout: async () => {
+    await signOut(auth);
+    set({ user: null, token: null });
+  },
+}));
+
+export default useAuthStore;
