396248daa10b4508a72e436970b49b5f59c276da
BosEriko <bos@eriko.ph>
2026-03-08 08:38:04 +0800

    Feature: Update Homepage to have User authentication flow

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,27 @@
+'use client';
+import { useEffect } from 'react';
+import useAuthStore from '@store/useAuthStore';
+import Atom from '@atom';
 import Template from "@template";
 import Link from "next/link";
 
-export default async function Home() {
+export default function Home() {
+  const { user, loading, logout } = useAuthStore();
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div>
-      <Link href="/api/discord/authentication/login">
-        <button>Login with Discord</button>
-      </Link>
+      <Atom.Visibility state={user === null}>
+        <Link href="/api/discord/authentication/login">
+          <button>Login with Discord</button>
+        </Link>
+      </Atom.Visibility>
+      <Atom.Visibility state={user !== null}>
+        <button onClick={() => logout()}>Log Out button here</button>
+      </Atom.Visibility>
     </div>
   );
 }
