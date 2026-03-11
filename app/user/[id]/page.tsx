3ab2d8285d6ca019320085eec7e1bad09b606ef0
BosEriko <bos@eriko.ph>
2026-03-11 19:03:29 +0800

    Feature: Pull Listing Data on Profile

--- app/user/[id]/page.tsx
+++ app/user/[id]/page.tsx
@@ -1,7 +1,34 @@
-export default function User() {
+import Template from "@template";
+
+import { collection, query, where, getDocs } from "firebase/firestore";
+import { db } from "@lib/Firebase";
+
+type PageProps = {
+  params: Promise<{
+    id: string;
+  }>;
+};
+
+export default async function User({ params }: PageProps) {
+  const { id } = await params;
+
+  const listingsQuery = query(
+    collection(db, "listings"),
+    where("userId", "==", id)
+  );
+
+  const snapshot = await getDocs(listingsQuery);
+
+  const listings = snapshot.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+  }));
+
   return (
-    <div>
-      user profile
-    </div>
-  )
+    <Template.Default>
+      <h1>User Profile</h1>
+
+      <pre>{JSON.stringify(listings, null, 2)}</pre>
+    </Template.Default>
+  );
 }
