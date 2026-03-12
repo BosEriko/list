bc86e2ed0954a5f8956463abde5e492df034f606
BosEriko <bos@eriko.ph>
2026-03-12 18:36:31 +0800

    Feature: Add Simple UI for Listing Page

--- app/list/[type]/[id]/[slug]/page.tsx
+++ app/list/[type]/[id]/[slug]/page.tsx
@@ -13,28 +13,54 @@ export default async function ListingPage({ params }: PageProps) {
   const { type, id } = await params;
   const listing = await getListingFromFirebase(id, type);
 
+  const sidebarContent = [
+    {
+      title: "Epsiodes",
+      content: listing.totalCount
+    },
+    {
+      title: "Score",
+      content: listing.score
+    }
+  ];
+
   return (
-    <Template.Default orientation="center">
-      <h1>{listing.title}</h1>
-
-      <img
-        src={listing.images.jpg.image_url}
-        alt={listing.title}
-        width={200}
-      />
-
-      <p><strong>Episodes:</strong> {listing.totalCount}</p>
-      <p><strong>Score:</strong> {listing.score}</p>
-      <p>{listing.synopsis}</p>
-
-      <Molecule.ListingEditor
-        itemId={id}
-        type={type}
-        title={listing.title}
-        count={0}
-        totalCount={listing.totalCount}
-        imageUrl={listing.images.jpg.small_image_url}
-      />
+    <Template.Default>
+      <div className="flex gap-3">
+        <div className="flex flex-col gap-3">
+          <div>
+            <img
+              className="w-300 rounded-md"
+              src={listing.images.jpg.image_url}
+              alt={listing.title}
+            />
+          </div>
+          <div>
+            <Molecule.ListingEditor
+              itemId={id}
+              type={type}
+              title={listing.title}
+              count={0}
+              totalCount={listing.totalCount}
+              imageUrl={listing.images.jpg.small_image_url}
+            />
+          </div>
+          <div className="bg-white border border-gray-200 rounded-md p-5 flex flex-col gap-3">
+            {sidebarContent.map((content, index) => (
+              <div className="flex flex-col gap-1" key={index}>
+                <div className="font-semibold text-gray-600 text-sm">{content.title}</div>
+                <div className="text-md">{content.content}</div>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="flex flex-col gap-3">
+          <h1 className="font-bold text-4xl">{listing.title}</h1>
+          <div className="bg-white border border-gray-200 rounded-md p-5">
+            {listing.synopsis}
+          </div>
+        </div>
+      </div>
     </Template.Default>
   );
 }
--- components/molecules/ListingEditor/ModalEditor.tsx
+++ components/molecules/ListingEditor/ModalEditor.tsx
@@ -122,7 +122,7 @@ const ModalEditor: React.FC<IModalEditorProps> = ({
     <Atom.Visibility state={user !== null}>
       <button
         onClick={() => setIsOpen(true)}
-        className="bg-green-500 text-white px-4 py-2 rounded"
+        className="bg-green-500 text-white px-4 py-2 rounded-md w-full"
       >
         + Add to List
       </button>
