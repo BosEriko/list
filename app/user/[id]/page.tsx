5b965419063206381a8501ddef7c799922d1e7ba
BosEriko <bos@eriko.ph>
2026-03-12 19:05:39 +0800

    Feature: Add Simple Layout to Profile

--- app/user/[id]/page.tsx
+++ app/user/[id]/page.tsx
@@ -39,33 +39,41 @@ export default async function User({ params }: PageProps) {
 
   return (
     <Template.Default>
-      <h1>User Profile</h1>
-      <table className="table-auto w-full border-separate border-spacing-y-2">
-        <thead>
-          <tr>
-            <th></th>
-            <th>Title</th>
-            <th>Progress</th>
-            <th>Status</th>
-          </tr>
-        </thead>
-        <tbody>
-          {listings.map((listing, key) => (
-            <Molecule.ListingEditor
-              itemId={listing.itemId}
-              type={listing.type}
-              title={listing.title}
-              count={listing.count}
-              totalCount={listing.totalCount}
-              imageUrl={listing.imageUrl}
-              listingUrl={listing.listingUrl}
-              status={listing.status}
-              isModal={false}
-              key={key}
-            />
-          ))}
-        </tbody>
-      </table>
+      <div className="flex flex-col lg:flex-row gap-6">
+        <div className="flex flex-col gap-4 lg:w-[300px] w-full">
+          <div className="w-full">
+            sidebar
+          </div>
+        </div>
+        <div className="flex flex-col gap-4 flex-1">
+          <table className="table-auto w-full border-separate border-spacing-y-2">
+            <thead>
+              <tr>
+                <th></th>
+                <th>Title</th>
+                <th>Progress</th>
+                <th>Status</th>
+              </tr>
+            </thead>
+            <tbody>
+              {listings.map((listing, key) => (
+                <Molecule.ListingEditor
+                  itemId={listing.itemId}
+                  type={listing.type}
+                  title={listing.title}
+                  count={listing.count}
+                  totalCount={listing.totalCount}
+                  imageUrl={listing.imageUrl}
+                  listingUrl={listing.listingUrl}
+                  status={listing.status}
+                  isModal={false}
+                  key={key}
+                />
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
     </Template.Default>
   );
 }
