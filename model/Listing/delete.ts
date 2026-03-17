db1b494f119a87d1de9b13a61b9b20922fdfa1ef
BosEriko <bos@eriko.ph>
2026-03-17 19:05:23 +0800

    Feature: Create Listing.delete

---
+++ model/Listing/delete.ts
@@ -0,0 +1,24 @@
+import { doc, deleteDoc } from "firebase/firestore";
+import { db } from "@lib/Firebase";
+
+const COLLECTION = "listings";
+
+interface IPayload {
+  userId: string;
+  itemId: string;
+  type: string;
+}
+
+const delete = async (payload: IPayload) => {
+  const docId = `${payload.userId}-${payload.type}-${payload.itemId}`;
+  const docRef = doc(db, COLLECTION, docId);
+
+  try {
+    await deleteDoc(docRef);
+    console.log(`Document ${docId} deleted successfully!`);
+  } catch (err) {
+    console.error(`Error deleting ${COLLECTION}:`, err);
+  }
+}
+
+export default delete;
