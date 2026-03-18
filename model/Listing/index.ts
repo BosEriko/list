02449f83ee7cf9956d17899e17a7efad93de52e8
BosEriko <bos@eriko.ph>
2026-03-18 11:26:26 +0800

    Feature: Add Note to Show rails pattern

--- model/Listing/index.ts
+++ model/Listing/index.ts
@@ -3,4 +3,13 @@ import read from "./read";
 import update from "./update";
 import delete from "./delete";
 
+// TODO: Follow rails pattern
+// - create
+// - find
+// - find_by
+// - where
+// - all
+// - update
+// - destroy (instead of selecting the model first, just pass id on destroy)
+
 export default { create, read, update, delete };
