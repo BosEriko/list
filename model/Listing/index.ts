b65a7ac1cb6bb73e2f22106af218d2977291ece7
BosEriko <bos@eriko.ph>
2026-03-18 15:40:32 +0800

    Feature: Remove import of unfinished methods

--- model/Listing/index.ts
+++ model/Listing/index.ts
@@ -1,17 +1,17 @@
 import all from "./all";
-import create from "./create";
-import destroy from "./destroy";
-import find from "./find";
-import find_by from "./find_by";
-import update from "./update";
-import where from "./where";
+// import create from "./create";
+// import destroy from "./destroy";
+// import find from "./find";
+// import find_by from "./find_by";
+// import update from "./update";
+// import where from "./where";
 
 export default {
   all,
-  create,
-  destroy,
-  find,
-  find_by,
-  update,
-  where
+  // create,
+  // destroy,
+  // find,
+  // find_by,
+  // update,
+  // where
 };
