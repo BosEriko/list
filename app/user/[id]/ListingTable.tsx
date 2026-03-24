11330cf5515e72ee8878ba3012aaf12fd41aead4
BosEriko <bos@eriko.ph>
2026-03-24 17:58:40 +0800

    Hotfix: Add strict type to userListings

--- app/user/[id]/ListingTable.tsx
+++ app/user/[id]/ListingTable.tsx
@@ -33,7 +33,7 @@ export default function ListingTable({ id }: ListingTableProps) {
       reset();
       setLoading(true);
       try {
-        let userListings = await Listing.where({ userId: id });
+        let userListings: Listing[] = await Listing.where({ userId: id });
         if (userListings.some((l) => l.type === "anime")) {
           const res = await fetch("/api/anime/ongoing");
           const data = await res.json();
