6d6d3ee23f1ccd5eef85a090de5a23d65d0f19d1
BosEriko <bos@eriko.ph>
2026-03-27 23:35:52 +0800

    Feature: Create constants for ITEM_ID_PATTERN and LISTING_ID_PATTERN

---
+++ config/initializers/constant/ITEM_ID_PATTERN.ts
@@ -0,0 +1,2 @@
+const ITEM_ID_PATTERN = /^(anime|manga|game)-[0-9]+$/;
+export default ITEM_ID_PATTERN;
---
+++ config/initializers/constant/LISTING_ID_PATTERN.ts
@@ -0,0 +1,2 @@
+const LISTING_ID_PATTERN = /^[0-9]+-(anime|manga|game)-[0-9]+$/;
+export default LISTING_ID_PATTERN;
