7df16e06537313286942622f63d4a2658632b712
BosEriko <bos@eriko.ph>
2026-04-01 10:00:04 +0800

    Hotfix: Add NextRequest Type to Listing routes

--- app/api/listings/[id]/route.ts
+++ app/api/listings/[id]/route.ts
@@ -1,17 +1,19 @@
+import { NextRequest } from "next/server";
 import ListingController from "@controller/Listing";
 
-export async function GET(req: Request, { params }: { params: { id: string } }) {
-  return ListingController.show_action(req, (await params).id);
+export async function GET(req: NextRequest, context: { params: Promise<{ id: string }> }): Promise<Response> {
+  const { id } = await context.params;
+  return ListingController.show_action(req, id);
 }
 
-export async function PATCH(req: Request, { params }: { params: { id: string } }) {
-  return ListingController.update_action(req, (await params).id);
+export async function PATCH(req: NextRequest, context: { params: Promise<{ id: string }> }): Promise<Response> {
+  return ListingController.update_action(req, id);
 }
 
-export async function PUT(req: Request, { params }: { params: { id: string } }) {
-  return ListingController.update_action(req, (await params).id);
+export async function PUT(req: NextRequest, context: { params: Promise<{ id: string }> }): Promise<Response> {
+  return ListingController.update_action(req, id);
 }
 
-export async function DELETE(req: Request, { params }: { params: { id: string } }) {
-  return ListingController.destroy_action(req, (await params).id);
+export async function DELETE(req: NextRequest, context: { params: Promise<{ id: string }> }): Promise<Response> {
+  return ListingController.destroy_action(req, id);
 }
